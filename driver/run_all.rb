0b75317accc1e42ee3fcdc94a7d274d114a3c716
Peter Anselmo <peter_anselmo@yahoo.com>
2014-09-28 22:54:59 -0400

    Change adding multiple rows to one SQL statment (big speedup), exponential growth

--- driver/run_all.rb
+++ driver/run_all.rb
@@ -10,38 +10,49 @@ def rand_str(length)
 end
 
 $dbh = Mysql.new CONFIG['host'], CONFIG['user'], CONFIG['pass'], CONFIG['database']
+
+def clear_log
+    web_log = CONFIG['www_path'] + 'log_time.txt'
+    `echo '' > #{web_log}`
+end
+
 def clear_rows
     sql = "DELETE FROM users"
     result = $dbh.query(sql);
 end
 
-def add_row
-    sql ="INSERT INTO users(username, fname, lname) VALUES ('#{rand_str(7)}', '#{rand_str(5)}', '#{rand_str(8)}')"
+def add_rows(num_rows=1)
+    sql ="INSERT INTO users(username, fname, lname) VALUES "
+    1.upto(num_rows) do
+        sql += " ('#{rand_str(7)}', '#{rand_str(5)}', '#{rand_str(8)}'),"
+    end
+    sql = sql[0..-2]
     result = $dbh.query(sql)
+    #result = `mysql -u #{CONFIG['user']} -p#{CONFIG['pass']} #{CONFIG['database']} -e "#{sql}"`
 end
 
 def double_rows
     sql = "SELECT COUNT(*) as num_rows FROM users"
     result = $dbh.query(sql);
     num_rows = result.fetch_row[0].to_i
-    1.upto(num_rows) do
-        add_row
-    end
+    add_rows(num_rows)
 end
 
 def run_benchmarks
     driver = Selenium::WebDriver.for :chrome
     driver.manage.timeouts.page_load = 300 # 5 minutes
-    100.times do
-        1000.times do
-            add_row
-        end
 
+    add_rows
+    driver.navigate.to 'http://localhost/web_performance/www/'
+
+    18.times do
+        double_rows
         driver.navigate.to 'http://localhost/web_performance/www/'
     end
     driver.quit
 end
 
+clear_log
 clear_rows
 
 run_benchmarks
